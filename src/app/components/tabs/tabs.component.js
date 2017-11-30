73feaca8f3ed10166f04056a8bc8d7a23b1fb433
FelipeUFRO15 <f.acuna08@ufromail.cl>
2017-11-30 12:37:49 -0300

    Prueba pestañas

--- src/app/components/tabs/tabs.component.js
+++ src/app/components/tabs/tabs.component.js
@@ -9,7 +9,34 @@
     controllerAs: 'vm'
   });
 
-  function tabCtrl() {
-    // var vm = this;
+  function tabCtrl($log) {
+    var vm = this;
+    vm.currentNavItem = 'page1';
+
+    vm.asignaturas = [
+      {
+        nombre: 'DESARROLLO DE APLICACIONES PARA INTERNET',
+        numero: 1,
+        link: 'apuntes'
+      },
+      {
+        nombre: 'REDES',
+        numero: 2,
+        link: 'actividades'
+      },
+      {
+        nombre: 'SISTEMAS DE INFORMACIÓN',
+        numero: 3,
+        link: 'trabajos'
+      }
+    ];
+
+    vm.goto = function (page) {
+      $log.log('Goto ' + page);
+    };
+
+    vm.agregarAsignatura = function () {
+      $log.log('agregarAsignatura()');
+    };
   }
 })();
